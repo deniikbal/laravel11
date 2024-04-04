aece0be83c32937541d94a6ba51eac120b85ce18
deniikbal <deniikbal@gmail.com>
2024-04-04 07:15:33 +0700

    ok

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+                    <div class="card-body">
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="bold">Nilai</h5>
+                            </div>
                         </div>
-                    @endif
+                        <div class="col-md-8">
+                            <div class="card-body">
 
-                    {{ __('You are logged in!') }}
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
